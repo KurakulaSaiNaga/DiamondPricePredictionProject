e8d881d694908c7442202e5eb440bc029fdbef8f
SaiNaga <124518357+KurakulaSaiNaga@users.noreply.github.com>
2025-05-08 18:36:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name='DiamonPricePrediction',
     version='0.0.1',
     author='SaiNaga',
-    author_email='sainaga@outlook.com',
+    author_email='ksainaga13@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 )
